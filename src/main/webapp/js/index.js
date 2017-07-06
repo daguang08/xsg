c392b43ed50916f6ee4394f7503255cd3895da54
unknown <daguang0822@126.com>
2017-07-07 00:01:47 +0800

    样式调整

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -4,16 +4,48 @@ require(['jquery', 'knockout', 'bootstrap', 'uui', 'director',"dataPicker","data
     var viewModel={
     		
     };
+    validateInput = function(id, value){
+		if(value == ""){
+			$("#"+id).focus();
+			return false;
+		}
+		//电话号码个数验证
+		if(id == "phone_num"){
+			var phone_num=$("#phone_num").val();
+			var RegExp = /^0?(13[0-9]|15[012356789]|17[013678]|18[0-9]|14[57])[0-9]{8}$/;
+			var telphoneRegExp=/^(0\\d{2}-\\d{8}(-\\d{1,4})?)|(0\\d{3}-\\d{7,8}(-\\d{1,4})?)$/;
+			if (RegExp.test(phone_num) == false ) {
+				$("#warning").html("号码格式不正确或者位数不正确");
+				$("#"+id).focus();
+				return false;
+			}
+			else{
+				$("#warning").html("");
+			}
+		}
+	};
     //保存方法
     viewModel.saveReservation = function() {
+    	var unit=$("#unit").val();
+    	var contacts=$("#contacts").val();
+    	var phone_num=$("#phone_num").val();
+    	var visit_date=$("#visit_date").val();
+    	var visit_num=$("#visit_num").val();
+    	var remark =$('#remark') .val();
+    	
+    	
+    
+    	
     	var options={};
-    	options["unit"]=$("#unit").val();
-    	options["contacts"] =$("#contacts").val();
-    	options["phone_num"] =$("#phone_num").val();
-    	options["visit_date"] =$("#visit_date").val();
-    	options["visit_num"] =$("#visit_num").val();
+    	options["unit"] = unit;
+    	options["contacts"] = contacts;
+    	options["phone_num"] = phone_num;
+    	options["visit_date"] = visit_date;
+    	options["visit_num"] = visit_num;
     	options["visit_time"] =$('#visit_time option:selected') .val();
     	options["remark"] =$('#remark') .val();
+    	
+    	
       	$.ajax({
     		url:"/xsg/reservation/save",
     		type: 'GET',
